0a627b7856fdfe313f6a4a195ce66c2448636871
prathibha-bollu <bprathibhaedu@gmail.com>
2025-09-06 13:19:32 +0300

    vite edit

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/MealPlan', // must match your repo name
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/MealPlan/' : '/', // <-- only use /MealPlan/ for production
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
-});
+}));
